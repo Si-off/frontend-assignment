2c35cb6b6be87802ed6e57e33e51372f319e258b
noismik <noismik97@gmail.com>
2024-05-25 13:27:41 +0900

    refactor: usePosition 위치값 처리방식 변경
    미리 계산하는 대신 해당 위치에 대한 값만 계산

--- src/hooks/usePosition.ts
+++ src/hooks/usePosition.ts
@@ -19,9 +19,8 @@ const usePosition = (
   targetRef: React.RefObject<HTMLElement>
 ) => {
   const getPosition = useCallback(
-    (dir: Direction, gap: number = 10) => {
+    (dir: Direction = 'bottom', gap: number = 10) => {
       if (!pivotRef.current || !targetRef.current) return { x: 0, y: 0 };
-
       const pivotRect = pivotRef.current.getBoundingClientRect();
       const targetRect = targetRef.current.getBoundingClientRect();
 
@@ -31,24 +30,34 @@ const usePosition = (
       const targetCenterX = targetRect.width / 2;
       const targetCenterY = targetRect.height / 2;
 
-      const directionMap: Record<Direction, { x: number; y: number }> = {
-        top: { x: pivotCenterX - targetCenterX, y: top - targetRect.height - gap },
-        topLeft: { x: left, y: top - targetRect.height - gap },
-        topRight: { x: right - targetRect.width, y: top - targetRect.height - gap },
-        right: { x: right + gap, y: pivotCenterY - targetCenterY },
-        rightTop: { x: right + gap, y: top },
-        rightBottom: { x: right + gap, y: bottom - targetRect.height },
-        bottom: { x: pivotCenterX - targetCenterX, y: bottom + gap },
-        bottomLeft: { x: left, y: bottom + gap },
-        bottomRight: { x: right - targetRect.width, y: bottom + gap },
-        left: { x: left - targetRect.width - gap, y: pivotCenterY - targetCenterY },
-        leftTop: { x: left - targetRect.width - gap, y: top },
-        leftBottom: { x: left - targetRect.width - gap, y: bottom - targetRect.height },
-      };
-
-      return (
-        directionMap[dir] || { x: pivotCenterX - targetCenterX, y: pivotCenterY - targetCenterY }
-      );
+      switch (dir) {
+        case 'top':
+          return { x: pivotCenterX - targetCenterX, y: top - targetRect.height - gap };
+        case 'topLeft':
+          return { x: left, y: top - targetRect.height - gap };
+        case 'topRight':
+          return { x: right - targetRect.width, y: top - targetRect.height - gap };
+        case 'right':
+          return { x: right + gap, y: pivotCenterY - targetCenterY };
+        case 'rightTop':
+          return { x: right + gap, y: top };
+        case 'rightBottom':
+          return { x: right + gap, y: bottom - targetRect.height };
+        case 'bottom':
+          return { x: pivotCenterX - targetCenterX, y: bottom + gap };
+        case 'bottomLeft':
+          return { x: left, y: bottom + gap };
+        case 'bottomRight':
+          return { x: right - targetRect.width, y: bottom + gap };
+        case 'left':
+          return { x: left - targetRect.width - gap, y: pivotCenterY - targetCenterY };
+        case 'leftTop':
+          return { x: left - targetRect.width - gap, y: top };
+        case 'leftBottom':
+          return { x: left - targetRect.width - gap, y: bottom - targetRect.height };
+        default:
+          return { x: pivotCenterX - targetCenterX, y: pivotCenterY - targetCenterY };
+      }
     },
     [pivotRef, targetRef]
   );
