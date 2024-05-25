26c3ba60e2452b43e4d93cc3bf6ac5bab78aa4f6
noismik <noismik97@gmail.com>
2024-05-25 12:56:43 +0900

    feat: usePosition 훅 gap 옵션 추가

--- src/hooks/usePosition.ts
+++ src/hooks/usePosition.ts
@@ -19,7 +19,7 @@ const usePosition = (
   tooltipRef: React.RefObject<HTMLElement>
 ) => {
   const getPosition = useCallback(
-    (dir: Direction) => {
+    (dir: Direction, gap: number = 10) => {
       const position = { x: 0, y: 0 };
       if (!targetRef.current || !tooltipRef.current) return position;
 
@@ -27,27 +27,31 @@ const usePosition = (
       const tooltipRect = tooltipRef.current.getBoundingClientRect();
 
       const { top, bottom, left, right, width, height } = targetRect;
-      const xCenter = left + width / 2;
-      const yCenter = top + height / 2;
+
+      const pivotCenterX = left + width / 2;
+      const pivotCenterY = top + height / 2;
+
+      const targetCenterX = tooltipRect.width / 2;
+      const targetCenterY = tooltipRect.height / 2;
 
       const directionMap: Record<Direction, [number, number]> = {
-        top: [xCenter - tooltipRect.width / 2, top - tooltipRect.height],
-        topLeft: [left, top - tooltipRect.height],
-        topRight: [right - tooltipRect.width, top - tooltipRect.height],
-        right: [right, yCenter - tooltipRect.height / 2],
-        rightTop: [right, top],
-        rightBottom: [right, bottom - tooltipRect.height],
-        bottom: [xCenter - tooltipRect.width / 2, bottom],
-        bottomLeft: [left, bottom],
-        bottomRight: [right - tooltipRect.width, bottom],
-        left: [left - tooltipRect.width, yCenter - tooltipRect.height / 2],
-        leftTop: [left - tooltipRect.width, top],
-        leftBottom: [left - tooltipRect.width, bottom - tooltipRect.height],
+        top: [pivotCenterX - targetCenterX, top - tooltipRect.height - gap],
+        topLeft: [left, top - tooltipRect.height - gap],
+        topRight: [right - tooltipRect.width, top - tooltipRect.height - gap],
+        right: [right + gap, pivotCenterY - targetCenterY],
+        rightTop: [right + gap, top],
+        rightBottom: [right + gap, bottom - tooltipRect.height],
+        bottom: [pivotCenterX - targetCenterX, bottom + gap],
+        bottomLeft: [left, bottom + gap],
+        bottomRight: [right - tooltipRect.width, bottom + gap],
+        left: [left - tooltipRect.width - gap, pivotCenterY - targetCenterY],
+        leftTop: [left - tooltipRect.width - gap, top],
+        leftBottom: [left - tooltipRect.width - gap, bottom - tooltipRect.height],
       };
 
       [position.x, position.y] = directionMap[dir] || [
-        xCenter - tooltipRect.width / 2,
-        yCenter - tooltipRect.height / 2,
+        pivotCenterX - targetCenterX,
+        pivotCenterY - targetCenterY,
       ];
 
       return position;
